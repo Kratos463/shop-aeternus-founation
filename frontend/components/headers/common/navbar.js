a0148742ac55a0f1f1cfa1df9cb8ffcb2614dcec
Abhishek <abhisheksunny463@gmail.com>
2024-05-30 09:49:34 +0530

    Solve the error while fatching products from navbar

--- frontend/components/headers/common/navbar.js
+++ frontend/components/headers/common/navbar.js
@@ -1,76 +1,13 @@
-<<<<<<< HEAD
 import React, { useState, useEffect, useContext } from "react";
-=======
-import React, { useState, useEffect, useContext, useMemo } from "react";
->>>>>>> 94a925ea93f7ff4019c9172b0df9b08ab15e6f38
 import Link from "next/link";
 import { Container, Row } from "reactstrap";
 import { useRouter } from "next/router";
-<<<<<<< HEAD
 import FilterContext from "../../../helpers/filter/FilterContext";
-=======
-import CategoryContext from '../../../helpers/category/CategoryContext';
-import FilterContext from '../../../helpers/filter/FilterContext';
-
-const getMenuItems = (categories, subcategoriesMap, router, setSelectedCategory, setSelectedCategoryId) => [
-  {
-    title: "Home",
-    type: "link",
-    path: '/'
-  },
-  {
-    title: "Products",
-    megaMenu: true,
-    megaMenuType: "small",
-    type: "sub",
-    children: Object.values(categories).flat().map(category => {
-      const subcategories = subcategoriesMap[category.Category_id]?.map(subcategory => {
-        return {
-          title: subcategory.Category,
-          type: "link",
-          icon: "icon-placeholder",
-          onClick: () => {
-            const path = `/shop/left_sidebar?category=${subcategory.Category}&brand=&color=&size=&minPrice=500&maxPrice=10000`;
-            setSelectedCategory(subcategory.Category);
-            setSelectedCategoryId(subcategory.Category_id);
-            router.push(path);
-          }
-        };
-      }) || [];
-      
-      return {
-        title: category.Category,
-        type: "sub",
-        children: subcategories
-      };
-    }),
-  },
-  {
-    title: "Blogs",
-    type: "link",
-    path: "/blogs/blog_right_sidebar",
-  },
-  {
-    title: "Contact",
-    type: "link",
-    path: "/page/account/contact",
-  },
-];
->>>>>>> 94a925ea93f7ff4019c9172b0df9b08ab15e6f38
 
 const NavBar = () => {
   const [navClose, setNavClose] = useState({ right: "0px" });
   const router = useRouter();
-<<<<<<< HEAD
   const { setSelectedCategory, setSelectedCategoryId } = useContext(FilterContext)
-=======
-  const { categories, subcategoriesMap } = useContext(CategoryContext);
-  const { setSelectedCategory, setSelectedCategoryId } = useContext(FilterContext);
-
-  const mainmenu = useMemo(() => {
-    return getMenuItems(categories, subcategoriesMap, router, setSelectedCategory, setSelectedCategoryId);
-  }, [categories, subcategoriesMap, router, setSelectedCategory, setSelectedCategoryId]);
->>>>>>> 94a925ea93f7ff4019c9172b0df9b08ab15e6f38
 
   useEffect(() => {
     if (window.innerWidth < 750) {
@@ -178,46 +115,9 @@ const NavBar = () => {
                   <a className="nav-link" onClick={openMblNav}>
                     {menuItem.title}
                     <span className="sub-arrow"></span>
-<<<<<<< HEAD
 
                   </a>
                 )}
-=======
-                  </a>
-                )}
-                {menuItem.children && !menuItem.megaMenu && (
-                  <ul className="nav-submenu">
-                    {menuItem.children.map((childrenItem, index) => (
-                      <li key={index} className={`${childrenItem.children ? "sub-menu " : ""}`}>
-                        {childrenItem.type === "sub" ? (
-                          <a href="#" onClick={() => toggletNavActive(childrenItem)}>
-                            {childrenItem.title}
-                            {childrenItem.tag === "new" && <span className="new-tag">new</span>}
-                            <i className="fa fa-angle-right ps-2"></i>
-                          </a>
-                        ) : (
-                          <a className="sub-menu-title" onClick={childrenItem.onClick || closeNav}>
-                            {childrenItem.title}
-                            {childrenItem.tag === "new" && <span className="new-tag">new</span>}
-                          </a>
-                        )}
-                        {childrenItem.children && (
-                          <ul className={`nav-sub-childmenu ${childrenItem.active ? "menu-open" : ""}`}>
-                            {childrenItem.children.map((childrenSubItem, key) => (
-                              <li key={key}>
-                                <a className="sub-menu-title" onClick={childrenSubItem.onClick || closeNav}>
-                                  {childrenSubItem.title}
-                                  {childrenSubItem.tag === "new" && <span className="new-tag">new</span>}
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                )}
->>>>>>> 94a925ea93f7ff4019c9172b0df9b08ab15e6f38
                 {menuItem.megaMenu && (
                   <div className={`mega-menu-container${menuItem.megaMenu ? "" : " opensubmenu"}`}>
                     <Container>
@@ -242,17 +142,10 @@ const NavBar = () => {
                                 <ul>
                                   {megaMenuItem.children.map((subMegaMenuItem, i) => (
                                     <li key={i}>
-<<<<<<< HEAD
                                       <Link href="#" onClick={() => clickCategory(subMegaMenuItem)}>
                                         <i className={`icon-${subMegaMenuItem.icon}`}></i>
                                         {subMegaMenuItem.Category}
                                       </Link>
-=======
-                                      <a onClick={subMegaMenuItem.onClick}>
-                                        <i className={`icon-${subMegaMenuItem.icon}`}></i>
-                                        {subMegaMenuItem.title}
-                                      </a>
->>>>>>> 94a925ea93f7ff4019c9172b0df9b08ab15e6f38
                                     </li>
                                   ))}
                                 </ul>
